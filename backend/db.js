13446e5283c877397de8d2879e2472fcf16e4578
Nilesh Kahlkar <nilkcode111@gmail.com>
2022-01-29 11:00:55 +0530

    connection db video-43

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,27 @@
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoseURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
-const connectToMongo =  () => {
-    mongoose.connect(mongoURI ,() => {
-      console.log("connected Mongo DB successfully")
+const connetToMongo = () => {
+
+    mongoose.connect(mongoseURI, () => {
+      console.log("conneted to  mongo successsfully");
     })
+
 }
 
-module.exports = connectToMongo; 
+
+module.exports = connetToMongo;
+
+
+
+
+
+/* Mongose deal with promises but in this project we used async and await function in javascript for
+request(req) and also response(resp) 
+
+Def:: async and await work like we initilize async at function and it await until promises resolve 
+complated resp or request
+
+
+*/
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,16 @@
-const connectToMongo = require('./db');
+const connetToMongo = require('./db');
 const express = require('express')
 
-connectToMongo();
+connetToMongo();
 
 
 const app = express()
 const port = 3000
 
-// Available routes
-
-// app.get('/', (req, res) => {
-//   res.send('Your mongo DB connected to 3000 Port Welcome snns')
-// })
-
-app.use(express.json())
-
-app.use('/api/auth/', require('./routes/auth'))
-
-app.use('/api/notes/', require('./routes/notes'))
+app.get('/', (req, res) => {
+  res.send('Hello Nilesh welcome to db!')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
