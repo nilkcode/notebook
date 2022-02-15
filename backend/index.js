f76dc5d50bfc4870e1d3f540192183ef91dc70ea
Nilesh Kahlkar <nilkcode111@gmail.com>
2022-02-15 09:12:33 +0530

    add user through thunder client

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ connetToMongo();
 
 
 const app = express()
-const port = 3000 
+const port = 5000 
 
 
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,27 +5,43 @@ const User = require('../models/User');
 // ...rest of the initial code omitted for simplicity.
 const { body, validationResult } = require('express-validator');
 
-// Create a user using :Post  "/api/auth/ " and does not requred Auth
+// Create a user using :Post "/api/auth/createuser" and does not requred Auth / No login required
 
-router.post('/',[
+router.post('/createuser',[
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Enter a valid passowrd').isLength({ min: 5 }),
 
-    ] ,(req, res) => {
+    ] ,async(req, res) => {
+ 
+    // If there are errors , return Bad request and the errors
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-     
-    User.create({
+
+    // check whether the user with this email exists already
+    try {
+     let user = await User.findOne({email:req.body.email});
+    
+     if (user){
+       
+        return res.status(400).json({error:"Sorry a user with this email already exists"})
+
+     } 
+     user  = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err)
-        res.json({error:'Please enter a unique value for email'})})
+      })
+     res.json({user})
+    }
+    catch(error){
+      console.error(error.message)
+    }
+      
+   
 
    
 })
